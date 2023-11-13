13a4298d68e0793b3c4d2d7ffb3ba0a3dab5de02
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-11-13 20:46:48 +0900

    merge layout sample

--- src/app/streaming/layout.tsx
+++ src/app/streaming/layout.tsx
@@ -1,28 +1,45 @@
-import { Suspense } from "react";
+import {Suspense} from "react";
 
 function Loading() {
   return <div>Loading...</div>;
 }
 
 export default function StreamingLayout({
-  children,
-}: {
+                                          children,
+                                        }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className={"grid grid-rows-3 grid-cols-3 gap-4"}>
-      {/* header */}
-      <div className={"row-span-1 col-span-3"}>
-        <p>here is header</p>
+      <div className="flex h-screen bg-green-300">
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <header className="flex justify-between items-center p-4">
+            <div className="flex">Left</div>
+            <div className="flex">Right</div>
+          </header>
+          <div className="flex h-full">
+            <nav className="flex w-72 h-full bg-pink-500">
+              <div className="w-full flex mx-auto px-6 py-8">
+                <div
+                    className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Sidebar
+                </div>
+              </div>
+            </nav>
+            <main className="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
+              <div className="flex w-full mx-auto px-6 py-8">
+                <div className="flex flex-col w-full h-full text-gray-900 text-xl border-4 border-gray-900 border-dashed">
+                  <Suspense fallback={<Loading/>}>{children}</Suspense>
+                </div>
+              </div>
+            </main>
+            <nav className="flex w-72 h-full bg-yellow-400">
+              <div className="w-full flex mx-auto px-6 py-8">
+                <div
+                    className="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Rightbar
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
-      {/* side */}
-      <div className={"row-span-2 col-span-1"}>
-        <p>here is side content</p>
-      </div>
-      {/* main */}
-      <div className={"row-span-2 col-span-2"}>
-        <Suspense fallback={<Loading />}>{children}</Suspense>
-      </div>
-    </div>
   );
 }
